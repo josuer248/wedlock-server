5b6384b1e6ce96e672d2b620d158ac8a18b9e89b
Josue <josue@MacBook-Pro-de-Josue.local>
2026-06-27 19:28:58 -0400

    fix port

--- webhook_server.py
+++ webhook_server.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, request
 import requests
 
-app = Flask(__name__)
+app = Flask(name)
 
-TELEGRAM_BOT_TOKEN = "8970384040:AAGPz9UKpVj7A2z9Gmc_GqmR848bc5Z40"
+TELEGRAM_BOT_TOKEN = "8970384040:AAGPz9UKpVj7A2z9Gmc___GqmR848bc5Z40"
 TELEGRAM_CHAT_ID = "-100430144920"
 
 def send_telegram(message):
@@ -15,11 +16,8 @@ def send_telegram(message):
 def webhook():
     data = request.json
     message = data.get("message", "Alert triggered!")
-    send_telegram(f"TradingView Alert: " + message)
-
-
+    send_telegram("TradingView Alert: " + message)
     return "OK", 200
 
-if __name__ == "__main__":
-    app.run(port=5000)
-
+if name == "main":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
